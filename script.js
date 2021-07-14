8947855ee016ae0f43f0f89c5a32b93ef0ddc8c4
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-07-14 10:31:39 -0400

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 const ball = document.querySelectorAll('.ball');
+const question = document.querySelector('#rgb-color');
+const start = document.querySelector('#reset-game');
+const paragraphStart = document.querySelector('#answer');
+const points = document.querySelector('#points');
+const correct = document.querySelector('.correct');
+const allBalls = document.querySelector('#all-balls')
 
 function getRandomColor() {
-  let numbers = '255';
   let color = 'rgb';
   color += '(' + (Math.floor(Math.random() * 255) + 1) + ', ' + (Math.floor(Math.random() * 255) + 1) + ', ' + (Math.floor(Math.random() * 255) + 1) + ')';
   return color;
 }
 
-for (index = 0; index < ball.length; index += 1) {
-  ball[index].style.backgroundColor = getRandomColor();
+function changeBallsColor() {
+  for (let index = 0; index < ball.length; index += 1) {
+    var ballIndex = ball[index];
+    ballIndex.style.backgroundColor = getRandomColor();
+  }
+  var number = Math.floor(Math.random() * 6);
+  var rightBall = ball[number];
+  rightBall.classList.add('correct');
+  question.innerText = rightBall.style.backgroundColor;
 }
+changeBallsColor();
+
+// fonte (com minhas modificações): https://github.com/tryber/sd-014-b-project-color-guess/pull/17/commits/5a856847b408d1b9d684980f64ed0a1ff0dfddbd
+function selectBall(ball) {
+  let right = document.querySelector('.correct').style.backgroundColor;
+  if (ball.target.style.backgroundColor === right) {
+    let answer = document.querySelector('#answer').innerText = 'Acertou!';
+    console.log(ball.target.style.backgroundColor);
+    console.log(right);
+  } else {
+    let answer = document.querySelector('#answer').innerText = 'Errou! Tente novamente!';
+    console.log(ball.target.style.backgroundColor);
+    console.log(right);
+  }
+}
+
+allBalls.addEventListener('click', selectBall);
+
+function gameStart(){
+  paragraphStart.innerText = "Escolha uma cor";
+  changeBallsColor();
+}
+
+start.addEventListener('click', gameStart);
