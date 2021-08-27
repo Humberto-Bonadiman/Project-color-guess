5b2d750d333429bdbe9a40c18265ad54600047a4
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-08-27 17:47:24 -0400

    Modificação do estilo

--- script.js
+++ script.js
@@ -7,14 +7,10 @@ const score = document.querySelector('#score');
 let correctAnswer = 0;
 
 function getRandomColor() {
-  let color = 'rgb';
-  const parentheses1 = '(';
-  const parentheses2 = ')';
-  const commaSpace = ', ';
-  color += parentheses1 + (Math.floor(Math.random() * 255) + 1) 
-  + commaSpace + (Math.floor(Math.random() * 255) + 1) 
-  + commaSpace + (Math.floor(Math.random() * 255) + 1) 
-  + parentheses2;
+  const randomNumber1 = Math.floor(Math.random() * 255) + 1;
+  const randomNumber2 = Math.floor(Math.random() * 255) + 1;
+  const randomNumber3 = Math.floor(Math.random() * 255) + 1;
+  const color = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
   return color;
 }
 
@@ -31,19 +27,15 @@ function changeBallsColor() {
 changeBallsColor();
 
 // fonte (com minhas modificações): https://github.com/tryber/sd-014-b-project-color-guess/pull/17/commits/5a856847b408d1b9d684980f64ed0a1ff0dfddbd
-function selectBall(ball) {
+function selectBall(item) {
   const right = document.querySelector('.correct').style.backgroundColor;
-  if (ball.target.style.backgroundColor === right) {
+  if (item.target.style.backgroundColor === right) {
     document.querySelector('#answer').innerText = 'Acertou!';
     correctAnswer += 3;
     const placar = 'Placar: ';
     score.innerText = placar + correctAnswer;
-    console.log(ball.target.style.backgroundColor);
-    console.log(right);
   } else {
     document.querySelector('#answer').innerText = 'Errou! Tente novamente!';
-    console.log(ball.target.style.backgroundColor);
-    console.log(right);
   }
 }
 
